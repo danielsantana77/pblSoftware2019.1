d03441dec6501bb63524cea746e889dbbdfb52ba
danielsantana77 <daniel_santana_oliveira@hotmail.com>
2019-08-09 09:50:08 -0300

    Update db.js

--- backend/models/db.js
+++ backend/models/db.js
@@ -1,11 +1,13 @@
 const Sequelize = require('sequelize')
 
+//Conexao com o Banco de Dados mysql
 const sequelize = new Sequelize('daniel' ,'daniel', 'Ecompuefs',{
     host: 'localhost',
     dialect: 'mysql'
 
 })
 
+//Inicia o servidor do Banco após a autenticação
 sequelize.authenticate().then(function(){
     console.log('Conexao com o Banco de Dados realizada com Sucesso')
 }).catch(function(err){
