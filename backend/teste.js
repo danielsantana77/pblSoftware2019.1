bc0821856e8a111e9b15682b4f4569912d0545b7
alexkidthunder <ribeiroo.grande@gmail.com>
2019-08-29 18:54:36 -0300

    Create Teste.js
    Testar o acesso ao BD por meio do sequelize, ele consegue acessar e ainda por cima salvar no BD

---
+++ backend/teste.js
@@ -0,0 +1,49 @@
+const Sequelize = require('sequelize')
+
+//Conexao com o Banco de Dados mysql
+
+//                                --BD--       --Usuario--     -Senha-
+const sequelize = new Sequelize('educatebd' ,'Administrador', 'educate',{
+    host: 'localhost',
+    dialect: 'mysql'
+
+})
+
+//Inicia o servidor do Banco após a autenticação
+sequelize.authenticate().then(function(){
+    console.log('Conexao com o Banco de Dados realizada com Sucesso');
+}).catch(function(err){
+    console.log('Erro ao realizar a conexao com o BD:' , err);
+});
+
+module.exports = {
+    Sequelize: Sequelize,
+    sequelize: sequelize
+}
+
+//Criando a tabela              //Nome da Tabela  
+const Admin = sequelize.define('Admins',{
+    username:{
+        type: Sequelize.STRING
+    },
+    password: {
+        type: Sequelize.STRING
+    }      
+})
+// Incluir o registro na tabela
+sequelize.sync().then(function () {
+    Admin.create({
+       username: 'foi',
+       password: '123'
+    })
+});
+
+// listar todos os registros da tabela Admin
+sequelize.sync().then(function () {
+    Admin.findAll().then(function(admin) {
+      console.log(admin)
+  });
+});
+// Sincroniza o model com o mysql , criando uma tabela no BD
+//Admin.sync({force: true})
+module.exports = Admin
