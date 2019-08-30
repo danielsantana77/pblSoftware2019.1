e1cbb853a59a5a14e68edc1b2247cf6ebfb1a594
alexkidthunder <ribeiroo.grande@gmail.com>
2019-08-30 09:16:50 -0300

    Backend renovado
    Login Funcional com autenticação
    
    Co-Authored-By: vdjmaciel <vdjmaciel@users.noreply.github.com>
    Co-Authored-By: tms00 <tms00@users.noreply.github.com>
    Co-Authored-By: mauriciomuniz <mauriciomuniz@users.noreply.github.com>

---
+++ newBack/config/database.js
@@ -0,0 +1,10 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': 'localhost',
+        'user': 'Administrador',
+        'password': 'educate'
+    },
+	'database': 'my_schema',
+    'users_table': 'users'
+};
